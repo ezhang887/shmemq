7c75ce26abb0bc7c21b7fc4e814e197042920694
Eric Zhang <ezhang887@gmail.com>
2019-11-25 20:05:44 -0800

    Update test example.

--- main.c
+++ main.c
@@ -1,22 +1,45 @@
 #include "shmemq.h"
 
 #include <stdio.h>
+#include <unistd.h>
+#include <assert.h>
 
-int main(){
-    int N = 12;
+static void push(int N){
     shmemq_t *q = shmemq_create("shmemq_test", N, sizeof(int));
 
-    for(int i=0; i<3*N; i++){
-        int a = i;
-        shmemq_push(q, &a);
+    for(int i=0; i<N; i++){
+        assert(shmemq_push(q, &i) == 0);
     }
 
-    for(int i=0; i<3*N; i++){
+    shmemq_destroy(q);
+}
+
+static void pull(int N){
+    shmemq_t *q = shmemq_create("shmemq_test", N, sizeof(int));
+
+    for(int i=0; i<N; i++){
         int a;
         if (shmemq_pull(q, &a) != -1){
-            printf("%d\n", a);
+            assert(a == i);
         }
     }
 
     shmemq_destroy(q);
+}
+
+int main(int argc, char *argv[]){
+
+    int N = 100000000;
+    
+    int child_pid = fork();
+    if (child_pid == -1){
+        perror("fork(): ");
+        return 1;
+    }
+    else if (child_pid == 0){
+        push(N);
+    }
+    else {
+        pull(N);
+    }
 }
